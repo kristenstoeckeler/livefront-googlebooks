dff8f291aa6678d5ba68823c4c09dd59a42325c9
Kristen Stoeckeler <kstoeckeler@gmail.com>
2024-04-28 20:36:08 -0500

    feat: adding some styles, added tests for BookDetailPage

--- src/features/books/BookDetailPage.jsx
+++ src/features/books/BookDetailPage.jsx
@@ -3,14 +3,20 @@ import { selectedBook } from './BookSlice';
 
 const BookDetail = () => {
     const selectBook = useSelector(selectedBook);
-    const { title, description, imageLinks }  = selectBook;
+    const { title, description, imageLinks } = selectBook;
 
   return(
-    <>
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <img alt="" src={imageLinks.thumbnail}/>
-    </>
-)};
+        <>
+          { selectBook.title && (
+            <div>
+              <h3 data-testid="title">{title}</h3>
+              <p data-testid="desc">{description}</p>
+              {imageLinks && (<img alt="" data-testid="image-links" src={imageLinks.thumbnail}/>)}
+            </div>
+          )};
+        </>
+
+    );
+};
 
 export default BookDetail;
---
+++ src/features/books/BookDetailPage.test.jsx
@@ -0,0 +1,82 @@
+import { render as rtlRender, screen } from '@testing-library/react'
+import configureStore from 'redux-mock-store';
+import { Provider, useSelector } from 'react-redux';
+import BookDetail from "./BookDetailPage";
+import booksReducer from '../books/BookSlice';
+
+
+
+jest.mock("react-redux", () => ({
+ ...jest.requireActual("react-redux"),
+ useSelector: jest.fn(),
+}));
+
+const mockStore = configureStore({reducer: booksReducer});
+const store = mockStore({
+        booksReducer,
+    });
+
+const render = component => rtlRender(
+    <Provider store={store}>
+        { component }
+    </Provider>
+);
+
+describe("BookDetail component", () => {
+
+    const mockAppState = {
+        booksReducer: {
+            books: [],
+            selectedBook: {
+                title: 'MockTitle',
+                description: 'MockDescription', 
+                imageLinks: {
+                    thumbnail: 'MockImageLinks' 
+                }
+            }
+
+        }
+    };
+
+    const mockEmptyState  = {
+        booksReducer: {
+            books: [],
+            selectedBook: {}
+        }
+    };
+
+    beforeEach(() => {
+        
+    });
+
+    afterEach(() => {
+        useSelector.mockClear();
+    });
+
+
+  it("should render BookDetail component", () => {
+    useSelector.mockImplementation(callback => {
+            return callback(mockAppState);
+        });
+    render(<BookDetail />);
+    const title = screen.getByTestId("title");
+    const desc = screen.getByTestId("desc");
+    const imageLinks = screen.getByTestId("image-links");
+    expect(title).toBeInTheDocument();
+    expect(desc).toBeInTheDocument();
+    expect(imageLinks).toBeInTheDocument();
+  });
+
+    it("should not render if no selected book ", () => {
+    useSelector.mockImplementation(callback => {
+            return callback(mockEmptyState);
+        });
+    render(<BookDetail />);
+    const title = screen.queryByTestId("title");
+    const desc = screen.queryByTestId("desc");
+    const imageLinks = screen.queryByTestId("image-links");
+    expect(title).toBeNull();
+    expect(desc).toBeNull();
+    expect(imageLinks).toBeNull();
+  });
+});
