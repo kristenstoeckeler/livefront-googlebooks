1ef1039e78a72dd4220d27379f73c07f3c698c89
Kristen Stoeckeler <kstoeckeler@gmail.com>
2024-04-29 16:07:34 -0500

    feat: fixing key prop warning in List

--- src/features/books/BookListPage.jsx
+++ src/features/books/BookListPage.jsx
@@ -1,7 +1,4 @@
-import {
-    useEffect,
-    useState
-} from 'react'
+import { useEffect } from 'react'
 import { 
     useDispatch,
     useSelector
@@ -24,41 +21,34 @@ const BookListPage  = () => {
     }, [dispatch, bookData]);
 
     const handleClick = (e) => {
-        console.log('in handleclick');
         dispatch(selectBook(e.volumeInfo))
     }
-
-    console.log('bookData', bookData)
     
   return (
-    <>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
             <h1>N.K. Jemisin's Complete Works</h1>
                 <ul>
                     { bookData?.length > 0 && bookData.map((book, index) => {
                         return(
-                            <>
-                                <h2 key={index}>
-                                    <li>
-                                        <img alt="" data-testid={"img"} src={book.volumeInfo.imageLinks.smallThumbnail}/>
-                                        <Link 
-                                            onClick={handleClick.bind(this, book)}
-                                            to={book.volumeInfo.title}
-                                            value={book.volumeInfo} 
-                                            data-testid={"link"}
-                                            >
-                                                {book.volumeInfo.title}
-                                        </Link>
-                                    </li>
-                                </h2>
-                            </>
+                            <h2 key={book.id}>
+                                <li>
+                                    <img alt="" data-testid={"img"} src={book.volumeInfo.imageLinks.smallThumbnail}/>
+                                    <Link 
+                                        onClick={handleClick.bind(this, book)}
+                                        to={book.volumeInfo.title}
+                                        value={book.volumeInfo} 
+                                        data-testid={"link"}
+                                        >
+                                            {book.volumeInfo.title}
+                                    </Link>
+                                </li>
+                            </h2>
                         )      
                     })}
                 </ul>
         </div>
     </main>
-    </>
   );
 }
 
