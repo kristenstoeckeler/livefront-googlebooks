089a556a011e242ede2ed016142733efe9f033e1
Kristen Stoeckeler <kstoeckeler@gmail.com>
2024-04-30 10:07:14 -0500

    feat:  tweaked styles

--- src/features/books/BookDetailPage.jsx
+++ src/features/books/BookDetailPage.jsx
@@ -6,24 +6,19 @@ const BookDetail = () => {
     const { title, description, imageLinks } = selectBook;
 
   return(
-        <>
-        <body>
+        <div className="page">
           <header />
           { selectBook.title && (
-            <div className="detail">
+            <>
               <h1 className="title" data-testid="title">{title.toUpperCase()}</h1>
               <div className="desc">
                 <img className="image" alt="" data-testid="img" src={imageLinks.thumbnail}/>
                 <p data-testid="desc">{description}</p>
               </div>
-            </div>
+            </>
           )};
           <footer/>
-
-        </body>
-          
-        </>
-
+        </div>
     );
 };
 
